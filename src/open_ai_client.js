b84de4c5e0f06d44a54ba223a1a8e43b2d58dff1
Tal Weinfeld <talw@webwise.co.il>
2025-07-07 12:16:22 +0300

    Prettier

--- src/open_ai_client.js
+++ src/open_ai_client.js
@@ -13,84 +13,89 @@ const DEFAULT_VERB = "POST";
 const API_URL_BASE = "https://api.openai.com/v1";
 
 export const OaiClientFactory = (apiKey) => {
-    return (pathAndVerb, body = {}) => {
-        const [verb, path] = [DEFAULT_VERB, ...pathAndVerb.split(" ")].slice(-2);
-        return kefir.fromNodeCallback((cb) =>
-            got({
-                method: verb,
-                url: [API_URL_BASE, path].join("/"),
-                headers: {
-                    Authorization: ["Bearer", apiKey].join(" "),
-                },
-                resolveBodyOnly: true,
-                responseType: "json",
-                json: body,
-            }).then(cb.bind(null, null), pipe(get("response.body"), JSON.parse, cb)),
-        );
-    };
+  return (pathAndVerb, body = {}) => {
+    const [verb, path] = [DEFAULT_VERB, ...pathAndVerb.split(" ")].slice(-2);
+    return kefir.fromNodeCallback((cb) =>
+      got({
+        method: verb,
+        url: [API_URL_BASE, path].join("/"),
+        headers: {
+          Authorization: ["Bearer", apiKey].join(" "),
+        },
+        resolveBodyOnly: true,
+        responseType: "json",
+        json: body,
+      }).then(cb.bind(null, null), pipe(get("response.body"), JSON.parse, cb)),
+    );
+  };
 };
 
 export const OaiStreamingClientFactory = (apiKey) => {
-    return function (path, body, verb = DEFAULT_VERB) {
-        const responseStream = got.stream({
-            method: verb,
-            url: [API_URL_BASE, path].join("/"),
-            headers: {
-                Authorization: ["Bearer", apiKey].join(" "),
-            },
-            json: body,
-        });
+  return function (path, body, verb = DEFAULT_VERB) {
+    const responseStream = got.stream({
+      method: verb,
+      url: [API_URL_BASE, path].join("/"),
+      headers: {
+        Authorization: ["Bearer", apiKey].join(" "),
+      },
+      json: body,
+    });
 
-        return kefir
-            .fromEvents(responseStream, "response")
-            .merge(kefir.fromEvents(responseStream, "error").map(get("response.body")).flatMap(kefir.constantError))
-            .take(1)
-            .flatMap((response) => {
-                const contentType = get("headers.content-type", response);
-                if (/^text\/event-stream/.test(contentType)) {
-                    const lineStream = pipeline(
-                        responseStream,
-                        split(/\r?\n/, null, { trailing: true }),
-                        noop,
-                    );
+    return kefir
+      .fromEvents(responseStream, "response")
+      .merge(
+        kefir
+          .fromEvents(responseStream, "error")
+          .map(get("response.body"))
+          .flatMap(kefir.constantError),
+      )
+      .take(1)
+      .flatMap((response) => {
+        const contentType = get("headers.content-type", response);
+        if (/^text\/event-stream/.test(contentType)) {
+          const lineStream = pipeline(
+            responseStream,
+            split(/\r?\n/, null, { trailing: true }),
+            noop,
+          );
 
-                    return kefir
-                        .merge([
-                            kefir.fromEvents(lineStream, "data"),
-                            kefir
-                                .fromEvents(lineStream, "error")
-                                .flatMap(kefir.constantError),
-                        ])
-                        .takeUntilBy(kefir.fromEvents(responseStream, "end").take(1))
-                        .bufferWhile((line) => line.length > 0)
-                        .flatten((buffer) => {
-                            const chunkMap = buffer.reduce((map, line) => {
-                                const { command, value } =
-                                line.match(/^(?<command>[^:]+):\s(?<value>.+)/)?.groups ?? {};
-                                if (command) map.set(command, (map.get(value) ?? "") + value);
-                                return map;
-                            }, new Map());
+          return kefir
+            .merge([
+              kefir.fromEvents(lineStream, "data"),
+              kefir
+                .fromEvents(lineStream, "error")
+                .flatMap(kefir.constantError),
+            ])
+            .takeUntilBy(kefir.fromEvents(responseStream, "end").take(1))
+            .bufferWhile((line) => line.length > 0)
+            .flatten((buffer) => {
+              const chunkMap = buffer.reduce((map, line) => {
+                const { command, value } =
+                  line.match(/^(?<command>[^:]+):\s(?<value>.+)/)?.groups ?? {};
+                if (command) map.set(command, (map.get(value) ?? "") + value);
+                return map;
+              }, new Map());
 
-                            return chunkMap.has("data")
-                                ? [
-                                    Object.fromEntries(
-                                        [...chunkMap.entries()].map(([key, value]) => [
-                                            key,
-                                            (key === "data" ? JSON.parse : identity)(value),
-                                        ]),
-                                    ),
-                                ]
-                                : [];
-                        });
-                } else {
-                    return kefir.fromNodeCallback((cb) =>
-                        pipeline(
-                            responseStream,
-                            concatStream(pipe(toString, JSON.parse, cb.bind(null, null))),
-                            cb,
-                        ),
-                    );
-                }
+              return chunkMap.has("data")
+                ? [
+                    Object.fromEntries(
+                      [...chunkMap.entries()].map(([key, value]) => [
+                        key,
+                        (key === "data" ? JSON.parse : identity)(value),
+                      ]),
+                    ),
+                  ]
+                : [];
             });
-    };
+        } else {
+          return kefir.fromNodeCallback((cb) =>
+            pipeline(
+              responseStream,
+              concatStream(pipe(toString, JSON.parse, cb.bind(null, null))),
+              cb,
+            ),
+          );
+        }
+      });
+  };
 };
